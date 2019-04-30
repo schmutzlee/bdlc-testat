e6d485b4b5147fb2b687c40d2e9f7a2f9c9045a4
Philip <bd01@cl-hpelb1-50-gw-01-lx-ub16.lxd>
2019-04-30 21:12:31 +0000

    fetch interval angepasst

--- files-fetch.sh
+++ files-fetch.sh
@@ -13,5 +13,5 @@ test -d ./data || mkdir ./data
 
 ## ##################################################
 ## get data
-curl -sSLG "https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=100&datatype=json&AccessKey=k-Fsn_DVInAzOQ4lk1AjcofhuXIr5xA0" | jq  -c '.'  >data/aapl.json 
-curl -sSLG "https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=100&datatype=json&AccessKey=k-Fsn_DVInAzOQ4lk1AjcofhuXIr5xA0" | jq  -c '.'  >data/amzn.json 
+curl -sSLG "https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=10&datatype=json&AccessKey=k-Fsn_DVInAzOQ4lk1AjcofhuXIr5xA0" | jq  -c '.'  >data/aapl.json 
+curl -sSLG "https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=10&datatype=json&AccessKey=k-Fsn_DVInAzOQ4lk1AjcofhuXIr5xA0" | jq  -c '.'  >data/amzn.json 
